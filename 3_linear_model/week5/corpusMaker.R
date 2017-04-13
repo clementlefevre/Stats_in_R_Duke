3947350038fc01dbdd266f4e3274e5cc2ed1fe3a
clementlefevre <clement.san@gmail.com>
2017-04-13 23:02:16 +0200

    Add Text Miner

---
+++ 3_linear_model/week5/corpusMaker.R
@@ -0,0 +1,34 @@
+
+
+corpusMaker<- function(df,feature){
+  corpus <- Corpus(VectorSource(df[,feature]))
+  corpus[[1]]$content
+  
+  corpus = tm_map(corpus, content_transformer(tolower))
+  
+  print(corpus[[1]]$content)
+  
+  # Remove punctuation
+  
+  corpus = tm_map(corpus, removePunctuation)
+  
+  
+  # Remove stopwords and apple
+  corpus = tm_map(corpus, removeWords, stopwords("english"))
+  
+  print(corpus[[1]]$content)
+  
+  # Stem document 
+  
+  corpus = tm_map(corpus, stemDocument)
+  
+  dtm <- TermDocumentMatrix(corpus)
+  
+  #dtm<- removeSparseTerms(dtm,sparse = .6)
+  
+  dtm<-as.data.frame(as.matrix(dtm))
+  colnames(dtm)<- make.names(colnames(dtm))
+  
+  return( dtm)
+  
+}
